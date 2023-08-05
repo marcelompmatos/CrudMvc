6d0a1c420736cb1e363c0fb3a0f862aca2e5b89d
Marcelo Matos <marcelompmatos@gmail.com>
2023-08-05 10:43:05 -0300

    Primeiro commit

---
+++ WebCRUDMVCSQL/Controllers/ProdutosController.cs
@@ -0,0 +1,165 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebCRUDMVCSQL.Models;
+
+namespace WebCRUDMVCSQL.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private readonly Contexto _context;
+
+        public ProdutosController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Produtos
+        public async Task<IActionResult> Index()
+        {
+            #region CARREGA PAGINA INDEX  
+            var produtos = (from m in _context.Produto
+                          join d in _context.Tipo_Produto on m.Id_Tipo_Produto equals (d.Id_Tipo_Produto)
+                          select new ProdutoViewModelIndex() { Nome = m.Nome, Descricao = d.Ds_Tipo_Produto, Id = m.Id }) ;
+            return View(await produtos.ToListAsync());
+            #endregion
+        }
+
+        // GET: Produtos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.Produto
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
+        }
+
+        // GET: Produtos/Create
+        public IActionResult Create()
+        {
+            #region CARRAGA A PAGINA QUE CRIA O PRODUTO
+            ViewData["Id_Tipo_Produto"] = new SelectList(_context.Tipo_Produto, "Id_Tipo_Produto", "Ds_Tipo_Produto");
+            return View();
+            #endregion
+        }
+
+        // POST: Produtos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Produto produto)
+        {
+            #region CRIA O PRODUTO
+            if (ModelState.IsValid)
+            {
+                _context.Add(produto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(produto);
+            #endregion
+        }
+
+        // GET: Produtos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            ViewData["Id_Tipo_Produto"] = new SelectList(_context.Tipo_Produto, "Id_Tipo_Produto", "Ds_Tipo_Produto");
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.Produto.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Produtos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Id_Tipo_Produto")] Produto produto)
+        {
+            if (id != produto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(produto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProdutoExists(produto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(produto);
+        }
+
+        // GET: Produtos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.Produto
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var produto = await _context.Produto.FindAsync(id);
+            _context.Produto.Remove(produto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProdutoExists(int id)
+        {
+            return _context.Produto.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebCRUDMVCSQL/Models/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebCRUDMVCSQL.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Tipo_Produto> Tipo_Produto { get; set; }
+
+    }
+}
